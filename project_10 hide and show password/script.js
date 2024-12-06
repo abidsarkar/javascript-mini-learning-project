5362b5bc38db96f99818fe34fdafd0ca8e5f68d0
abidsarkar <abidsarkar0@gmail.com>
2024-12-06 22:09:43 +0600

    finish

--- project_10 hide and show password/script.js
+++ project_10 hide and show password/script.js
@@ -4,7 +4,11 @@ let password = document.getElementById('password');
 eyeIcon.onclick= ()=>{
     if(password.type == "password"){
         password.type = "text";
+        eyeIcon.src ="img/eye-line.png"
+        
     }else{
         password.type = "password";
+        eyeIcon.src ="img/eye-off-line.png"
     }
+    
 }
