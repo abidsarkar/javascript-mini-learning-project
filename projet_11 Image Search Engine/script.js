9d0a28e250136e8eccda5e583f9da7c74fe58e16
abidsarkar <abidsarkar0@gmail.com>
2024-12-07 21:07:07 +0600

    get the element by id

---
+++ projet_11 Image Search Engine/script.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementById("searchForm");
+const SearchBox = document.getElementById("searchBox");
+const SearchResult = document.getElementById("searchResult");
+const ShowMoreBtn = document.getElementById("showMoreBtn");
