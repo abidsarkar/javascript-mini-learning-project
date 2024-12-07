be01b3d11eb9bb28a160034458b8fee034f5757f
abidsarkar <abidsarkar0@gmail.com>
2024-12-07 21:39:00 +0600

    show more option added

--- projet_11 Image Search Engine/script.js
+++ projet_11 Image Search Engine/script.js
@@ -1,4 +1,4 @@
-const searchForm = document.getElementById("searchForm");
+const SearchForm = document.getElementById("searchForm");
 const SearchBox = document.getElementById("searchBox");
 const SearchResult = document.getElementById("searchResult");
 const ShowMoreBtn = document.getElementById("showMoreBtn");
@@ -6,7 +6,7 @@ let keyword = "birds";
 let page = 1;
 let key = "a5d4238f515992d21f853e054a976abdc2467ea35799c7fea1e208f78e8b0644";
 async function searchImage(keyword) {
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}&per_page=12`;
   try{
     const response = await fetch(url);
     const data = await response.json();
@@ -17,17 +17,24 @@ async function searchImage(keyword) {
         const imageLink = document.createElement("a");
         imageLink.href = result.links.html;
         imageLink.target = "_blank";
+        imageLink.appendChild(image);
+        SearchResult.appendChild(imageLink);
     });
+    ShowMoreBtn.style.display = "block";
   }
   catch(error){
     console.error('Error fetching images:', error);
   }
   
 }
-searchForm.addEventListener("submit", (e) => {
+SearchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
   let value = SearchBox.value;
 //   console.log(value);
   searchImage(value);
 });
+
+ShowMoreBtn.addEventListener(("click"),()=>{
+    page++;
+})
