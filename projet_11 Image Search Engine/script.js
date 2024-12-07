37591b16374a7653cc3618a65397a1525fa0b2a7
abidsarkar <abidsarkar0@gmail.com>
2024-12-07 21:30:39 +0600

    show the img

--- projet_11 Image Search Engine/script.js
+++ projet_11 Image Search Engine/script.js
@@ -2,3 +2,32 @@ const searchForm = document.getElementById("searchForm");
 const SearchBox = document.getElementById("searchBox");
 const SearchResult = document.getElementById("searchResult");
 const ShowMoreBtn = document.getElementById("showMoreBtn");
+let keyword = "birds";
+let page = 1;
+let key = "a5d4238f515992d21f853e054a976abdc2467ea35799c7fea1e208f78e8b0644";
+async function searchImage(keyword) {
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}`;
+  try{
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.results;
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+    });
+  }
+  catch(error){
+    console.error('Error fetching images:', error);
+  }
+  
+}
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  let value = SearchBox.value;
+//   console.log(value);
+  searchImage(value);
+});
