0496e3ad1bc4ec8bde86a9ad0957629af8441065
abidsarkar <abidsarkar0@gmail.com>
2024-12-14 12:16:20 +0600

    get the random number in a let password

--- project_13 Random Password Generator/script.js
+++ project_13 Random Password Generator/script.js
@@ -4,3 +4,11 @@ const upperCase = "ABCDEFGHIjKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "!@#$%";
+function createPassword(){
+    let password = "";
+    password +=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password +=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password +=number[Math.floor(Math.random()*number.length)];
+    password +=symbol[Math.floor(Math.random()*symbol.length)];
+    
+}
