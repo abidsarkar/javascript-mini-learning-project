e51d7d21128263619c402865e4b455f90358d083
abidsarkar <abidsarkar0@gmail.com>
2024-12-14 12:13:44 +0600

    const the random value for pass

---
+++ project_13 Random Password Generator/script.js
@@ -0,0 +1,6 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+const upperCase = "ABCDEFGHIjKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%";
