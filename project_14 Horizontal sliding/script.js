a613577d5ef0657f6ea1d1f8ea216e0102d67ce1
abidsarkar <abidsarkar0@gmail.com>
2024-12-17 19:54:46 +0600

    scroll option on mouse

--- project_14 Horizontal sliding/script.js
+++ project_14 Horizontal sliding/script.js
@@ -1,3 +1,7 @@
 let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
