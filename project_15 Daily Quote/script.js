4f3f8fa3bd9ffddf8bc81d625ba835383dca5391
abidsarkar <abidsarkar0@gmail.com>
2024-12-22 19:36:17 +0600

    get the document

---
+++ project_15 Daily Quote/script.js
@@ -0,0 +1,4 @@
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+const share = document.querySelector(".share");
+const cpy = document.querySelector(".cpy");
