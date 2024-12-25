66425b135ce0a960ee7527f46bc1ecd9f8710369
abidsarkar <abidsarkar0@gmail.com>
2024-12-25 16:25:39 +0600

    working the api

--- project_15 Daily Quote/script.js
+++ project_15 Daily Quote/script.js
@@ -2,3 +2,20 @@ const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 const share = document.querySelector(".share");
 const cpy = document.querySelector(".cpy");
+async function getQuote() {
+    const api = "https://quotes-api-self.vercel.app/quote";
+    // const response = await fetch("https://quotes-api-self.vercel.app/quote");
+    try{}
+    catch{
+
+    }
+    const data = await response.json();
+    console.log(response);
+    console.log(data);
+    console.log(data.quote);
+    console.log(data.author);
+    quote.innerHTML = data.quote;
+    author.innerHTML = data.author;
+  }
+
+  getQuote();
