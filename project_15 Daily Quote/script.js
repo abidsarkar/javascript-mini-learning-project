ba4377ef10d69ae06d3c52198c580b7c0642ba6d
abidsarkar <abidsarkar0@gmail.com>
2024-12-25 16:29:57 +0600

    both try catch and cpy option added

--- project_15 Daily Quote/script.js
+++ project_15 Daily Quote/script.js
@@ -1,21 +1,46 @@
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 const share = document.querySelector(".share");
-const cpy = document.querySelector(".cpy");
-async function getQuote() {
-    const api = "https://quotes-api-self.vercel.app/quote";
-    // const response = await fetch("https://quotes-api-self.vercel.app/quote");
-    try{}
-    catch{
 
+async function getQuote() {
+  const api = "https://quotes-api-self.vercel.app/quote";
+
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(response);
-    console.log(data);
-    console.log(data.quote);
-    console.log(data.author);
+    // console.log(response);
+    // console.log(data);
+    // console.log(data.quote);
+    // console.log(data.author);
     quote.innerHTML = data.quote;
     author.innerHTML = data.author;
+  }  catch (error) {
+    console.error("Error fetching weather data:", error);
+    quote.textContent = "Failed to fetch quote data. Please try again.";
+    author.textContent = "";
   }
+}
 
-  getQuote();
+getQuote();
+
+// copy the quote
+const cpy = document.querySelector(".cpy");
+cpy.addEventListener('click', async () => {
+    try {
+      const text = passwordBox.value;
+      if(text===''){
+        // console.log("unsuccess");
+      }
+      else{
+        await navigator.clipboard.writeText(text);
+        // console.log("success");
+      }
+      
+    } catch (err) {
+      console.error("Failed to copy to clipboard:", err);
+      // Show error message (optional)
+    }
+  });
