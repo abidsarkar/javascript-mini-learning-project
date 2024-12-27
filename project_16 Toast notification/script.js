3a180fa8f1167af86e8bb1c641b7a3ddb7d0b2a3
abidsarkar <abidsarkar0@gmail.com>
2024-12-27 21:26:03 +0600

    move the onclick to js file as moduler js did not support it and in js addeventL as querySelectorAll and loop it

--- project_16 Toast notification/script.js
+++ project_16 Toast notification/script.js
@@ -1,8 +1,12 @@
 let toastBox = document.getElementById('toastBox');
+let buttons = document.querySelectorAll('.showToast');
+buttons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        console.log("toast");
+        let toast = document.createElement("div");
+        toast.classList.add('toast');
+        toast.innerHTML = 'success';
+        toastBox.appendChild(toast);
+    })
+})
 
-function showToast() {
-  let toast = document.createElement("div");
-  toast.classList.add('toast');
-  toast.innerHTML = 'success';
-  toastBox.appendChild(toast);
-}
