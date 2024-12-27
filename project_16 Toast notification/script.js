1276c4992a41938da757f0ea2e53191bf4037d92
abidsarkar <abidsarkar0@gmail.com>
2024-12-27 21:39:06 +0600

    show toast message according to the button

--- project_16 Toast notification/script.js
+++ project_16 Toast notification/script.js
@@ -2,10 +2,11 @@ let toastBox = document.getElementById('toastBox');
 let buttons = document.querySelectorAll('.showToast');
 buttons.forEach(button =>{
     button.addEventListener('click',()=>{
-        console.log("toast");
+        // console.log("toast");
+        let message = button.getAttribute('data-message');
         let toast = document.createElement("div");
         toast.classList.add('toast');
-        toast.innerHTML = 'success';
+        toast.innerHTML = message;
         toastBox.appendChild(toast);
     })
 })
