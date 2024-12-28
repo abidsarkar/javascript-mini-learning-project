d987b1cfd1d2e867fb95439555316be22e2c0500
abidsarkar <abidsarkar0@gmail.com>
2024-12-28 21:01:36 +0600

    comment added

--- project_16 Toast notification/script.js
+++ project_16 Toast notification/script.js
@@ -4,9 +4,19 @@ buttons.forEach(button =>{
     button.addEventListener('click',()=>{
         // console.log("toast");
         let message = button.getAttribute('data-message');
+        let icon = button.getAttribute('data-icon');
+        let str;
+        if(icon==='success'){
+            str="<i class="fa-solid fa-circle-check"></i>";
+        }
         let toast = document.createElement("div");
+        // add the class toast
         toast.classList.add('toast');
-        toast.innerHTML = message;
+        //<i class="fa-solid fa-circle-check"></i>
+        // <i class="fa-solid fa-circle-xmark"></i>
+        //<i class="fa-solid fa-circle-exclamation"></i>
+        
+        toast.innerHTML =`${icon}--${message}`;
         toastBox.appendChild(toast);
     })
 })
