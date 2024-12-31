2ff9c59f34b7254a9c663f8b219c0bcf18f5ddb0
abidsarkar <abidsarkar0@gmail.com>
2024-12-31 19:58:05 +0600

    change the p tag to textarea

--- project_17 Notes App/script.js
+++ project_17 Notes App/script.js
@@ -2,10 +2,13 @@ const notesContainer = document.querySelector(".notesContainer");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".inputBox");
 createBtn.addEventListener("click", () => {
-  let inputBox = document.createElement("p");
+  let inputBox = document.createElement("div");
+  let textarea = document.createElement("textarea");
   let i = document.createElement("i");
   inputBox.className = "inputBox";
-  inputBox.setAttribute("contenteditable", "true");
+  textarea.placeholder = "Write your note here...";
   i.classList.add("fa-solid", "fa-trash-can");
-  notesContainer.appendChild(inputBox).appendChild(i);
+  inputBox.appendChild(textarea);
+  inputBox.appendChild(i);
+  notesContainer.appendChild(inputBox);
 });
