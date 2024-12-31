5ef21b5b0fc5812309ea0e3f87b943d8c043eba8
abidsarkar <abidsarkar0@gmail.com>
2024-12-31 19:40:04 +0600

    click  to creat input box from js file

--- project_17 Notes App/script.js
+++ project_17 Notes App/script.js
@@ -1,3 +1,11 @@
 const notesContainer = document.querySelector(".notesContainer");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".inputBox");
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let i = document.createElement("i");
+  inputBox.className = "inputBox";
+  inputBox.setAttribute("contenteditable", "true");
+  i.classList.add("fa-solid", "fa-trash-can");
+  notesContainer.appendChild(inputBox).appendChild(i);
+});
