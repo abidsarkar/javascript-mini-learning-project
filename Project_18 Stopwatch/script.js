c80ecc27431f0e3fb78672aa2c9769f0d1b4d952
abidsarkar <abidsarkar0@gmail.com>
2025-01-08 20:16:40 +0600

    stop btn working

--- Project_18 Stopwatch/script.js
+++ Project_18 Stopwatch/script.js
@@ -12,11 +12,10 @@ function stopwatch() {
       hours++;
     }
   }
-  let h =  hours <10 ? '0'+hours : hours;
-  let m =  minutes <10 ? '0'+minutes : minutes;
-  let s =  seconds <10 ? '0'+seconds : seconds;
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
   displayTime.innerHTML = h + ":" + m + ":" + s;
-
 }
 
 function watchStart() {
@@ -29,7 +28,21 @@ function watchStart() {
 let stop = document.getElementById("stop");
 let start = document.getElementById("start");
 let reset = document.getElementById("reset");
-
+//start btn
 start.addEventListener("click", () => {
   watchStart();
 });
+//stop btn
+function watchStop() {
+  clearInterval(timer);
+}
+stop.addEventListener("click", () => {
+    watchStop();
+});
+//reset btn
+function watchReset() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  displayTime.innerHTML = "00:00:00";
+}
+reset.addEventListener
