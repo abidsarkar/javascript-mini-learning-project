3509b3335aa23f9d987c74224528c47ec66d58b3
abidsarkar <abidsarkar0@gmail.com>
2025-01-13 17:07:11 +0600

    play btn in logic

--- project_19 music Player app/script.js
+++ project_19 music Player app/script.js
@@ -1,1 +1,11 @@
-let abc = 0;
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let playBtn = document.getElementById("playBtn");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+playBtn.addEventListener("click", () => {
+    
+});
