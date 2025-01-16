ec9d0572baf4b90b52bf49dc9afb4ea6432f2910
abidsarkar <abidsarkar0@gmail.com>
2025-01-16 20:27:18 +0600

    song play pause added

--- project_19 music Player app/script.js
+++ project_19 music Player app/script.js
@@ -6,6 +6,17 @@ song.onloadedmetadata = function () {
   progress.max = song.duration;
   progress.value = song.currentTime;
 };
-playBtn.addEventListener("click", () => {
-    
+//play pause btn
+let isPlaying = false; // Track the current playback state
+
+playBtn.addEventListener('click', () => {
+  isPlaying = !isPlaying; // Toggle isPlaying state on each click
+
+  if (isPlaying) {
+    playBtn.src = './img/pause-circle-fill.svg';
+    song.play(); // Play the song if image is switched to pause icon
+  } else {
+    playBtn.src = './img/play-circle-fill.svg';
+    song.pause(); // Pause the song if image is switched to play icon
+  }
 });
