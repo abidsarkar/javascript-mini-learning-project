a26c3b2990b9b1553c585d31e8cf2bb97a11a900
abidsarkar <abidsarkar0@gmail.com>
2025-01-16 20:37:02 +0600

    play pause from progress bar

--- project_19 music Player app/script.js
+++ project_19 music Player app/script.js
@@ -9,14 +9,28 @@ song.onloadedmetadata = function () {
 //play pause btn
 let isPlaying = false; // Track the current playback state
 
-playBtn.addEventListener('click', () => {
+playBtn.addEventListener("click", () => {
   isPlaying = !isPlaying; // Toggle isPlaying state on each click
 
   if (isPlaying) {
-    playBtn.src = './img/pause-circle-fill.svg';
+    playBtn.src = "./img/pause-circle-fill.svg";
     song.play(); // Play the song if image is switched to pause icon
+    isPlaying = true;
   } else {
-    playBtn.src = './img/play-circle-fill.svg';
+    playBtn.src = "./img/play-circle-fill.svg";
     song.pause(); // Pause the song if image is switched to play icon
+    isPlaying = false;
   }
 });
+//range bar
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+progress.onchange =function(){
+    song.play();
+    song.currentTime = progress.value;
+    playBtn.src = "./img/pause-circle-fill.svg";
+    isPlaying = true;
+}
