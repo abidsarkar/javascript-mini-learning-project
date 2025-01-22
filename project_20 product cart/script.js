16a94c885d93d8fd191c6b30a69cde24634b4a58
abidsarkar <abidsarkar0@gmail.com>
2025-01-22 22:32:09 +0600

    toggle btn color

--- project_20 product cart/script.js
+++ project_20 product cart/script.js
@@ -1,11 +1,21 @@
 let productImg = document.getElementById("productImg");
 let btn = document.getElementsByClassName("btn");
-btn[0].addEventListener(("click"),()=>{
-    productImg.src = './img/image.png';
-})
-btn[1].addEventListener(("click"),()=>{
-    productImg.src = './img/image2.png';
-})
-btn[2].addEventListener(("click"),()=>{
-    productImg.src = './img/image3.png';
-})
+btn[0].addEventListener("click", () => {
+  productImg.src = "./img/image.png";
+
+  btn[0].classList.add("active");
+  btn[1].classList.remove("active");
+  btn[2].classList.remove("active");
+});
+btn[1].addEventListener("click", () => {
+  productImg.src = "./img/image2.png";
+  btn[0].classList.remove("active");
+  btn[1].classList.add("active");
+  btn[2].classList.remove("active");
+});
+btn[2].addEventListener("click", () => {
+  productImg.src = "./img/image3.png";
+  btn[0].classList.remove("active");
+  btn[1].classList.remove("active");
+  btn[2].classList.add("active");
+});
