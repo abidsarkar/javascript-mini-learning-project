ac7b9874c78b77449242edbf0f480fb183e52ef9
abidsarkar <abidsarkar0@gmail.com>
2025-02-14 19:57:25 +0600

    Add error message elements to form validation script

---
+++ project_25 from validation/script.js
@@ -0,0 +1,7 @@
+let nameError = document.getElementById("nameError");
+let phoneError = document.getElementById("phoneError");
+let emailError = document.getElementById("emailError");
+let messageError = document.getElementById("messageError");
+let submitError = document.getElementById("submitError");
+
+
